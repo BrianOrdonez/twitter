41b9887a2f1b91451d2fa112432ab0821294e8b7
flakeater <brianordonez6994@gmail.com>
2021-09-24 14:52:35 -0400

    Update App.vue

--- faketwitter/src/App.vue
+++ faketwitter/src/App.vue
@@ -2,9 +2,17 @@
   <router-view />
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+});
 </script>
+
+<style lang="scss">
+$fa-font-path: "~@fortawesome/fontawesome-free/webfonts";
+@import "~@fortawesome/fontawesome-free/scss/fontawesome";
+@import "~@fortawesome/fontawesome-free/scss/solid"; // fas
+@import "~@fortawesome/fontawesome-free/scss/regular"; // far
+@import "~@fortawesome/fontawesome-free/scss/brands"; // fab
+</style>
